26765bcb7a7b0c494d6b92fe6a9a08c8f52aa76d
xoomdev <xoomdevnet@gmail.com>
2013-01-13 00:07:43 -0600

    DeviceInfo: Add chipset to supplement cpu display in device info
    Use ro.device.chipset to display chipset name along with cpu info
    in device info settings.
    
    For example:
    ro.device.chipset=Qualcomm Snapdragon S4 Pro APQ8064
    ro.device.cpu=1.5GHz quad-core Krait
    
    With the Krait, a15, a9, plus dual-cores/quad-cores AND
    the various names Exynos, OMAP, Snapdragom msmXXXX, etc
    its all fun info, but its a bit much for one line.
    
    Separate them out so we can display it all but not
    exceed twitter's 140 character limit.
    
    Change-Id: I5c2c58d16d0e7fa1344d6290a67e1281c710c3fb

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -65,6 +65,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_MOD_VERSION = "mod_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
+    private static final String KEY_DEVICE_CHIPSET = "device_chipset";
     private static final String KEY_DEVICE_CPU = "device_cpu";
     private static final String KEY_DEVICE_GPU = "device_gpu";
     private static final String KEY_DEVICE_MEMORY = "device_memory";
@@ -102,6 +103,8 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
         findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
 
+        addStringPreference(KEY_DEVICE_CHIPSET,
+                SystemProperties.get("ro.device.chipset", null));
         addStringPreference(KEY_DEVICE_CPU,
                 SystemProperties.get("ro.device.cpu", getCPUInfo()));
         addStringPreference(KEY_DEVICE_GPU,
