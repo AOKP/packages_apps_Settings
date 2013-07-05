fe8a138fde07e77c981d7c0b4d787e316ad6a3a9
Jubakuba <Jubakuba@aokp.co>
2013-07-04 20:18:57 -0600

    Settings: AutoSmsReply while in QuietHours [WIP]
    TODO: Get/Add/Make settings/strings
          Test
    
    Change-Id: Iaa8e0488411b7024f4bd72fcdd64be52c840924f

---
+++ src/com/android/settings/service/BootService.java
@@ -0,0 +1,57 @@
+package com.android.settings.service;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+
+public class BootService extends Service {
+
+    public static boolean servicesStarted = false;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent == null) {
+            stopSelf();
+        }
+        new BootWorker(this).execute();
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    class BootWorker extends AsyncTask<Void, Void, Void> {
+
+        Context c;
+
+        public BootWorker(Context c) {
+            this.c = c;
+        }
+
+        @Override
+        protected Void doInBackground(Void... args) {
+
+            if (SmsService.returnUserAutoSms(c) != 0) {
+                c.startService(new Intent(c, SmsService.class));
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            servicesStarted = true;
+            stopSelf();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
