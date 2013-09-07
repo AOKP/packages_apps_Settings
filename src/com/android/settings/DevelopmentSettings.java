e9ea8cc554a068ac822c4ddcc2b89e30d91e9475
JBirdVegas <jbirdvegas@gmail.com>
2013-09-07 02:58:32 -0400

    Settings: Allow Changing logging status
    Logging has a performance cost. Considering most users have no interest
    in development, debugging or logging allow enabling and disabling of
    system and debug logcat output. This is the configuration settings.
    
    Change-Id: Ie6b26ae7fb867a4212f4d34da3bd03162d599b0c

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -130,6 +130,8 @@ public class DevelopmentSettings extends PreferenceFragment
     private static final String DEBUG_DEBUGGING_CATEGORY_KEY = "debug_debugging_category";
     private static final String DEBUG_APPLICATIONS_CATEGORY_KEY = "debug_applications_category";
 
+    private static final String CHANGE_LOGGING_STATUS_KEY = "change_logging_status";
+
     private static final String OPENGL_TRACES_KEY = "enable_opengl_traces";
 
     private static final String IMMEDIATELY_DESTROY_ACTIVITIES_KEY
@@ -170,6 +172,7 @@ public class DevelopmentSettings extends PreferenceFragment
     private CheckBoxPreference mWaitForDebugger;
     private CheckBoxPreference mVerifyAppsOverUsb;
 
+    private CheckBoxPreference mChangeLoggingStatus;
     private CheckBoxPreference mStrictMode;
     private CheckBoxPreference mPointerLocation;
     private CheckBoxPreference mShowTouches;
@@ -311,6 +314,18 @@ public class DevelopmentSettings extends PreferenceFragment
         if (hdcpChecking != null) {
             mAllPrefs.add(hdcpChecking);
         }
+        mChangeLoggingStatus = (CheckBoxPreference) findPreference(
+                CHANGE_LOGGING_STATUS_KEY);
+        mChangeLoggingStatus.setChecked(Log.getStatus() == Log.ALLOW_LOGGING);
+        mChangeLoggingStatus.setOnPreferenceClickListener(
+                new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Log.setLogability(getActivity(),
+                        mChangeLoggingStatus.isEnabled() ? 1 : 0);
+                return true;
+            }
+        });
         removeHdcpOptionsForProduction();
     }
 
