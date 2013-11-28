d3ce3d76f215f876fa8b7cbe620b4873db7763d8
helicopter88 <johnlemon.lol@gmail.com>
2013-11-28 23:40:58 +0100

    Settings: Security: implement a pin keypad shuffler [2/2]
    this is to further prevent people using the tablet guessing your pin based on fingerprints left on the screen
    example of the expected outcome - https://www.dropbox.com/sh/r3xb2q53ksaqni9/Oz1Eu8Dm7O
    
    PatchSet:
    - fix checkbox was wrong assigned
    - enable only for PIN Password lock
    - small cleanup of old code
    
    PatchSet:
    - cleanup
    
    PatchSet:
    - add modes.....disabled/always/on request
    
    Change-Id: I225af5654c440aa16964df3257130febec6ef275

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -86,6 +86,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String KEY_MANAGE_TRUST_AGENTS = "manage_trust_agents";
     private static final String KEY_ADVANCED_REBOOT = "advanced_reboot";
     private static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "quick_unlock_control";
+    private static final String LOCK_NUMPAD_RANDOM = "lock_numpad_random";
 
     private static final int SET_OR_CHANGE_LOCK_METHOD_REQUEST = 123;
     private static final int CONFIRM_EXISTING_FOR_BIOMETRIC_WEAK_IMPROVE_REQUEST = 124;
@@ -136,6 +137,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private SwitchPreference mPowerButtonInstantlyLocks;
     private ListPreference mAdvancedReboot;
     private SwitchPreference mQuickUnlockScreen;
+    private ListPreference mLockNumpadRandom;
 
     private ListPreference mSmsSecurityCheck;
 
@@ -310,6 +312,16 @@ public class SecuritySettings extends SettingsPreferenceFragment
         if (mQuickUnlockScreen != null) {
             mQuickUnlockScreen.setChecked(Settings.Secure.getInt(getContentResolver(),
                     Settings.Secure.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 1) == 1);
+        }
+
+        // Lock Numpad Random
+        mLockNumpadRandom = (ListPreference) root.findPreference(LOCK_NUMPAD_RANDOM);
+        if (mLockNumpadRandom != null) {
+            mLockNumpadRandom.setValue(String.valueOf(
+                    Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCK_NUMPAD_RANDOM, 0)));
+            mLockNumpadRandom.setSummary(mLockNumpadRandom.getEntry());
+            mLockNumpadRandom.setOnPreferenceChangeListener(this);
         }
 
         // Append the rest of the settings
@@ -759,6 +771,12 @@ public class SecuritySettings extends SettingsPreferenceFragment
             Settings.Secure.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.Secure.LOCKSCREEN_QUICK_UNLOCK_CONTROL,
                     (Boolean) value ? 1 : 0);
+        } else if (preference == mLockNumpadRandom) {
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.LOCK_NUMPAD_RANDOM,
+                    Integer.valueOf((String) value));
+            mLockNumpadRandom.setValue(String.valueOf(value));
+            mLockNumpadRandom.setSummary(mLockNumpadRandom.getEntry());
         }
         return result;
     }
