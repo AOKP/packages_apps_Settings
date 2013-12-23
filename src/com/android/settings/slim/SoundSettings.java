d703b4a8b0624bd794275a5ba9e0bd744567c364
Muhammed Nazim <mnm9994u@gmail.com>
2013-12-23 18:22:59 +0100

    Option to use volume keys to control media volume anytime [2/2]
    The volume keys control the Media volume rather than the Ringer
    hence removing the issue of accidentaly changing the Ringer volume.
    
    Credits: Pawitp
    
    Change-Id: Ic0a1c04ae41eac65e9535d6de5f11c0456730315

--- src/com/android/settings/slim/SoundSettings.java
+++ src/com/android/settings/slim/SoundSettings.java
@@ -41,9 +41,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_SAFE_HEADSET_VOLUME = "safe_headset_volume";
     private static final String PREF_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
+    private static final String KEY_VOL_MEDIA = "volume_keys_control_media_stream";
 
     private SwitchPreference mSafeHeadsetVolume;
     private ListPreference mAnnoyingNotifications;
+    private SwitchPreference mVolumeKeysControlMedia;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,6 +65,10 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         mAnnoyingNotifications.setValue(Integer.toString(notificationThreshold));
         mAnnoyingNotifications.setOnPreferenceChangeListener(this);
 
+        mVolumeKeysControlMedia = (SwitchPreference) findPreference(KEY_VOL_MEDIA);
+        mVolumeKeysControlMedia.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.VOLUME_KEYS_CONTROL_MEDIA_STREAM, 0) != 0);
+        mVolumeKeysControlMedia.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -90,6 +96,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements
             final int val = Integer.valueOf((String) objValue);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, val);
+        }
+        if (KEY_VOL_MEDIA.equals(key)) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.VOLUME_KEYS_CONTROL_MEDIA_STREAM,
+                    (Boolean) objValue ? 1 : 0);
         }
         return true;
     }
