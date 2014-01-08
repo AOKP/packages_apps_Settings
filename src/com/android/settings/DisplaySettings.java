c9a2ddc16a6a195ddbfa9bc32142bf1e505b9fe7
Zhang Fang <zfang@codeaurora.org>
2014-01-08 23:03:21 +0800

    Settings: Add screen color settings
    Add "Screen Color" item in settings.
    This function support set Hue, Saturation, Contrast and Intensity value,
    and screen color will be changed by this settings.
    
    Change-Id: If6f6e7ee7f28862476a8b955cd3252175fefa2a1
    
    Settings : Fix CustomScreenColor when picking an image
    
    Change-Id: I510b562eb0e9fb9b558380db5c5d6e818ba17d12
    
    Settings: Start service as explicit intent
    
    Needed for API level 21. Add some ressource bits, set text color to
    white.
    
    Change-Id: I9c4a9139be69fa5c6d6c74055b5475f0d930c93a

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -36,6 +36,8 @@ import android.app.Dialog;
 import android.app.admin.DevicePolicyManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.SharedPreferences;
@@ -99,6 +101,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String CATEGORY_ADVANCED = "advanced_display_prefs";
     private static final String KEY_DISPLAY_COLOR = "color_calibration";
     private static final String KEY_DISPLAY_GAMMA = "gamma_tuning";
+    private static final String KEY_SCREEN_COLOR_SETTINGS = "screencolor_settings";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -108,6 +111,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String ROTATION_ANGLE_270 = "270";
 
     private PreferenceScreen mDisplayRotationPreference;
+    private PreferenceScreen mScreenColorSettings;
 
     private FontDialogPreference mFontSizePref;
 
@@ -242,6 +246,10 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
                 getPreferenceScreen(), KEY_SCREEN_OFF_GESTURE_SETTINGS);
 
+        mScreenColorSettings = (PreferenceScreen) findPreference(KEY_SCREEN_COLOR_SETTINGS);
+        if (!isPostProcessingSupported()) {
+            getPreferenceScreen().removePreference(mScreenColorSettings);
+        }
     }
 
     private static boolean allowAllRotations(Context context) {
@@ -618,6 +626,17 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 Log.d(TAG, "Color enhancement settings restored.");
             }
         }
+    }
+
+    private boolean isPostProcessingSupported() {
+        boolean ret = true;
+        final PackageManager pm = getPackageManager();
+        try {
+            pm.getPackageInfo("com.qualcomm.display", PackageManager.GET_META_DATA);
+        } catch (NameNotFoundException e) {
+            ret = false;
+        }
+        return ret;
     }
 
     private static boolean isAdaptiveBacklightSupported() {
