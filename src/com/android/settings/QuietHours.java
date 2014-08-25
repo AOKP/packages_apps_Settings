26ff4817393b6bf0b9ef1b4ac2b4883af3f69526
task650 <task650@gmail.com>
2014-08-25 12:17:44 -0700

    Add System sound option for quiet hours (2/2)
    Change-Id: If68ccee1aa23cd93326c414d209db151bfbc2347

--- src/com/android/settings/QuietHours.java
+++ src/com/android/settings/QuietHours.java
@@ -33,6 +33,8 @@ public class QuietHours extends SettingsPreferenceFragment implements
 
     private static final String KEY_QUIET_HOURS_ENABLED = "quiet_hours_enabled";
 
+    private static final String KEY_QUIET_HOURS_SYSTEM = "quiet_hours_system";
+
     private static final String KEY_QUIET_HOURS_NOTIFICATIONS = "quiet_hours_notifications";
     
     private static final String KEY_QUIET_HOURS_RINGER = "quiet_hours_ringer";
@@ -44,6 +46,8 @@ public class QuietHours extends SettingsPreferenceFragment implements
     private static final String KEY_QUIET_HOURS_TIMERANGE = "quiet_hours_timerange";
 
     private CheckBoxPreference mQuietHoursEnabled;
+
+    private CheckBoxPreference mQuietHoursSystem;
 
     private CheckBoxPreference mQuietHoursNotifications;
     
@@ -73,6 +77,7 @@ public class QuietHours extends SettingsPreferenceFragment implements
             mQuietHoursRinger = (CheckBoxPreference) prefSet.findPreference(KEY_QUIET_HOURS_RINGER);
             mQuietHoursStill = (CheckBoxPreference) prefSet.findPreference(KEY_QUIET_HOURS_STILL);
             mQuietHoursDim = (CheckBoxPreference) findPreference(KEY_QUIET_HOURS_DIM);
+            mQuietHoursSystem = (CheckBoxPreference) findPreference(KEY_QUIET_HOURS_SYSTEM);
 
             // Set the preference state and listeners where applicable
             mQuietHoursEnabled.setChecked(Settings.AOKP.getInt(resolver, Settings.AOKP.QUIET_HOURS_ENABLED, 0) == 1);
@@ -115,6 +120,10 @@ public class QuietHours extends SettingsPreferenceFragment implements
         } else if (preference == mQuietHoursDim) {
             Settings.AOKP.putInt(resolver, Settings.AOKP.QUIET_HOURS_DIM,
                     mQuietHoursDim.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mQuietHoursSystem) {
+            Settings.AOKP.putInt(resolver, Settings.AOKP.QUIET_HOURS_SYSTEM,
+                    mQuietHoursSystem.isChecked() ? 1 : 0);
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
