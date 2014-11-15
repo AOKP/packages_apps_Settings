27d005b6ca7562c1de7a0b8fc3607fc5bb811395
Lars Greiss <kufikugel@googlemail.com>
2014-11-15 06:17:22 +0100

    Settings: use SwitchPreference instead of CheckBoxPreference
    be consistent....lp is using now SwitchPreferences
    
    Change-Id: I794b699e9bc7819efc02886064121e0992c7214a

--- src/com/android/settings/slim/DisplayRotation.java
+++ src/com/android/settings/slim/DisplayRotation.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The CyanogenMod Project
+ * Copyright (C) 2014 SlimRoms Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,10 +21,10 @@ import android.content.Context;
 import android.database.ContentObserver;
 import android.os.Bundle;
 import android.os.Handler;
-import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.internal.view.RotationPolicy;
@@ -40,11 +41,11 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
     private static final String ROTATION_180_PREF = "display_rotation_180";
     private static final String ROTATION_270_PREF = "display_rotation_270";
 
-    private CheckBoxPreference mAccelerometer;
-    private CheckBoxPreference mRotation0Pref;
-    private CheckBoxPreference mRotation90Pref;
-    private CheckBoxPreference mRotation180Pref;
-    private CheckBoxPreference mRotation270Pref;
+    private SwitchPreference mAccelerometer;
+    private SwitchPreference mRotation0Pref;
+    private SwitchPreference mRotation90Pref;
+    private SwitchPreference mRotation180Pref;
+    private SwitchPreference mRotation270Pref;
 
     public static final int ROTATION_0_MODE = 1;
     public static final int ROTATION_90_MODE = 2;
@@ -66,13 +67,14 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
-        mAccelerometer = (CheckBoxPreference) findPreference(KEY_ACCELEROMETER);
+        mAccelerometer = (SwitchPreference) findPreference(KEY_ACCELEROMETER);
         mAccelerometer.setPersistent(false);
+        mAccelerometer.setOnPreferenceChangeListener(this);
 
-        mRotation0Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_0_PREF);
-        mRotation90Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_90_PREF);
-        mRotation180Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_180_PREF);
-        mRotation270Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_270_PREF);
+        mRotation0Pref = (SwitchPreference) prefSet.findPreference(ROTATION_0_PREF);
+        mRotation90Pref = (SwitchPreference) prefSet.findPreference(ROTATION_90_PREF);
+        mRotation180Pref = (SwitchPreference) prefSet.findPreference(ROTATION_180_PREF);
+        mRotation270Pref = (SwitchPreference) prefSet.findPreference(ROTATION_270_PREF);
 
         int mode = Settings.System.getInt(getContentResolver(),
                         Settings.System.ACCELEROMETER_ROTATION_ANGLES,
@@ -82,6 +84,11 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
         mRotation90Pref.setChecked((mode & ROTATION_90_MODE) != 0);
         mRotation180Pref.setChecked((mode & ROTATION_180_MODE) != 0);
         mRotation270Pref.setChecked((mode & ROTATION_270_MODE) != 0);
+
+        mRotation0Pref.setOnPreferenceChangeListener(this);
+        mRotation90Pref.setOnPreferenceChangeListener(this);
+        mRotation180Pref.setOnPreferenceChangeListener(this);
+        mRotation270Pref.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -109,27 +116,32 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
         mAccelerometer.setChecked(!RotationPolicy.isRotationLocked(getActivity()));
     }
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
-    }
-
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
         if (preference == mAccelerometer) {
             RotationPolicy.setRotationLockForAccessibility(getActivity(),
-                !mAccelerometer.isChecked());
+                !((Boolean) objValue));
         } else if (preference == mRotation0Pref ||
                 preference == mRotation90Pref ||
                 preference == mRotation180Pref ||
                 preference == mRotation270Pref) {
             int mode = 0;
-            if (mRotation0Pref.isChecked())
+            if (preference == mRotation0Pref && ((Boolean) objValue)
+                    || preference != mRotation0Pref && mRotation0Pref.isChecked()) {
                 mode |= ROTATION_0_MODE;
-            if (mRotation90Pref.isChecked())
+            }
+            if (preference == mRotation90Pref && ((Boolean) objValue)
+                    || preference != mRotation90Pref && mRotation90Pref.isChecked()) {
                 mode |= ROTATION_90_MODE;
-            if (mRotation180Pref.isChecked())
+            }
+            if (preference == mRotation180Pref && ((Boolean) objValue)
+                    || preference != mRotation180Pref && mRotation180Pref.isChecked()) {
                 mode |= ROTATION_180_MODE;
-            if (mRotation270Pref.isChecked())
+            }
+            if (preference == mRotation270Pref && ((Boolean) objValue)
+                    || preference != mRotation270Pref && mRotation270Pref.isChecked()) {
                 mode |= ROTATION_270_MODE;
+            }
             if (mode == 0) {
                 mode |= ROTATION_0_MODE;
                 mRotation0Pref.setChecked(true);
@@ -138,6 +150,7 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
                     Settings.System.ACCELEROMETER_ROTATION_ANGLES, mode);
             return true;
         }
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+        return false;
     }
+
 }
