4604d3a8220e517a470dbef7a909ec8403b0b8bd
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-11-18 15:29:21 -0500

    Settings: Conditionally remove Buttons tile from Dashboard
    Change-Id: Iaddd3dd87ea2b0b04e3f312a156a6a25a948d039
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1338,6 +1338,12 @@ public class SettingsActivity extends Activity
                     if (!supported) {
                         removeTile = true;
                     }
+                } else if (id == R.id.button_settings) {
+                    boolean hasDeviceKeys = getResources().getInteger(
+                            com.android.internal.R.integer.config_deviceHardwareKeys) != 0;
+                    if (!hasDeviceKeys) {
+                        removeTile = true;
+                    }
                 }
 
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
