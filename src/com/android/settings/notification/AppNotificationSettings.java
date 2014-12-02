a9fcc3548e9c95eaf5f16abae670cb0c9ba7e237
Lars Greiss <kufikugel@googlemail.com>
2014-12-02 13:18:29 +0100

    Settings: Update AppNotificationSettings if user resumes
    Right now if a user resumes this activty (eg from recents) and did change in the
    mean time settings which effects the visibility of preferences in AppNotificationSettings
    (eg were removed) it does not get updated.
    
    Fix it with simply reloading the content on onResume and decide to show the entries
    on the current user settings situation.
    
    Change-Id: I0b16ebf35604fdca1611f026c9b609427f3060c8

--- src/com/android/settings/notification/AppNotificationSettings.java
+++ src/com/android/settings/notification/AppNotificationSettings.java
@@ -24,6 +24,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
@@ -109,6 +110,21 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = getActivity();
+        refreshSettings();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshSettings();
+    }
+
+    private void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
         Intent intent = getActivity().getIntent();
         if (DEBUG) Log.d(TAG, "onCreate getIntent()=" + intent);
         if (intent == null) {
@@ -137,6 +153,8 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
         }
 
         addPreferencesFromResource(R.xml.app_notification_settings);
+        prefs = getPreferenceScreen();
+
         mBlock = (SwitchPreference) findPreference(KEY_BLOCK);
         mPriority = (SwitchPreference) findPreference(KEY_PRIORITY);
         mSensitive = (SwitchPreference) findPreference(KEY_SENSITIVE);
@@ -147,7 +165,7 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
         final boolean enabled = getLockscreenNotificationsEnabled();
         final boolean allowPrivate = getLockscreenAllowPrivateNotifications();
         if (!secure || !enabled || !allowPrivate) {
-            getPreferenceScreen().removePreference(mSensitive);
+            prefs.removePreference(mSensitive);
         }
 
         mAppRow = NotificationAppList.loadAppRow(pm, info.applicationInfo, mBackend);
@@ -234,14 +252,15 @@ public class AppNotificationSettings extends SettingsPreferenceFragment {
         final boolean isSystemPkg = Utils.isSystemPackage(pm, info);
 
         if (isSystemPkg || !getLockscreenNotificationsEnabled()) {
-            getPreferenceScreen().removePreference(mShowNoOngoingOnKeyguard);
-            getPreferenceScreen().removePreference(mShowOnKeyguard);
+            prefs.removePreference(mShowNoOngoingOnKeyguard);
+            prefs.removePreference(mShowOnKeyguard);
         }
 
         if (isSystemPkg) {
-            getPreferenceScreen().removePreference(mBlock);
+            prefs.removePreference(mBlock);
             mPriority.setDependency(null); // don't have it depend on a preference that's gone
         }
+
     }
 
     private boolean getLockscreenNotificationsEnabled() {
