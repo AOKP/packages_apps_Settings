4f3c9886cebd46171c8ddaae08342a5e8d487292
LorDClockaN <davor@losinj.com>
2015-02-05 14:38:48 +0100

    REVERT Dialer and Camera wiget hide
    Change-Id: I78050fa9b5f3c03ae1710201dd0c4aed939e19d1

--- src/com/android/settings/aicp/VariousShit.java
+++ src/com/android/settings/aicp/VariousShit.java
@@ -19,7 +19,6 @@ package com.android.settings.aicp;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.admin.DevicePolicyManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -98,8 +97,6 @@ public class VariousShit extends SettingsPreferenceFragment
 
     private static final int REQUEST_PICK_BOOT_ANIMATION = 201;
 
-    private static final String KEY_LOCKSCREEN_CAMERA_WIDGET_HIDE = "camera_widget_hide";
-    private static final String KEY_LOCKSCREEN_DIALER_WIDGET_HIDE = "dialer_widget_hide";
     private static final String KEY_LOCKSCREEN_WEATHER = "lockscreen_weather";
     private static final String PREF_CUSTOM_BOOTANIM = "custom_bootanimation";
     private static final String BOOTANIMATION_SYSTEM_PATH = "/system/media/bootanimation.zip";
@@ -113,8 +110,6 @@ public class VariousShit extends SettingsPreferenceFragment
     // Package name of the cLock app
     public static final String LOCKCLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
 
-    private SwitchPreference mCameraWidgetHide;
-    private SwitchPreference mDialerWidgetHide;
     private SwitchPreference mLockscreenWeather;
     private SwitchPreference mProximityWake;
     private PreferenceScreen mVariousShitScreen;
@@ -186,26 +181,6 @@ public class VariousShit extends SettingsPreferenceFragment
         } else {
             mVariousShitScreen.removePreference(mHiddenShitUnlocked);
             mVariousShitScreen.removePreference(mHiddenImg);
-        }
-
-        // Camera widget hide
-        mCameraWidgetHide = (SwitchPreference) findPreference(KEY_LOCKSCREEN_CAMERA_WIDGET_HIDE);
-        boolean mCameraDisabled = false;
-        DevicePolicyManager dpm =
-            (DevicePolicyManager) getActivity().getSystemService(Context.DEVICE_POLICY_SERVICE);
-        if (dpm != null) {
-            mCameraDisabled = dpm.getCameraDisabled(null);
-        }
-        if (mCameraDisabled){
-            mVariousShitScreen.removePreference(mCameraWidgetHide);
-        }
-
-        // Dialer widget hide
-        mDialerWidgetHide = (SwitchPreference) findPreference(KEY_LOCKSCREEN_DIALER_WIDGET_HIDE);
-        boolean IsVoiceCapable = res.getBoolean(
-                com.android.internal.R.bool.config_voice_capable);
-        if (!IsVoiceCapable) {
-            mVariousShitScreen.removePreference(mDialerWidgetHide);
         }
 
         // Lockscreen weather
