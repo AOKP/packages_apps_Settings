80c221bb969877a128cf6da84a0335f3a0c55d8b
LorDClockaN <davor@losinj.com>
2015-02-16 19:51:57 +0100

    Revert "Remove Doze category if doze not available"
    This reverts commit f7568f151e5e74d49f52a69997221a21d41ff1d5.
    
    Change-Id: I392e22104528a4d49b1f0341344011f5145efa14

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -104,7 +104,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_SCREEN_OFF_GESTURE_SETTINGS = "screen_off_gesture_settings";
 
     private static final String CATEGORY_ADVANCED = "advanced_display_prefs";
-    private static final String CATEGORY_DOZE = "category_doze_options";
     private static final String KEY_DISPLAY_COLOR = "color_calibration";
     private static final String KEY_DISPLAY_GAMMA = "gamma_tuning";
     private static final String KEY_SCREEN_COLOR_SETTINGS = "screencolor_settings";
@@ -235,7 +234,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 mDozeTimeout.setOnPreferenceChangeListener(this);
             }
         } else {
-            removePreference(CATEGORY_DOZE);
+            removePreference(KEY_DOZE);
+            removePreference(KEY_DOZE_TIMEOUT);
         }
 
         if (RotationPolicy.isRotationSupported(activity)) {
