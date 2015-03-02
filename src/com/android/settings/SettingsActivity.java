85177efb6274f359d20e3d2247c23486dc4cec39
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-02 12:06:24 +0000

    QS: add Ambient display tile
    * add Ambient display settings activity for long press on tile
    
    Drawables thanks to Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: Id52c668d7d73d66feac7282861459bd15972f4ac

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -88,6 +88,7 @@ import com.android.settings.dashboard.NoHomeDialogFragment;
 import com.android.settings.dashboard.SearchResultsSummary;
 import com.android.settings.deviceinfo.Memory;
 import com.android.settings.deviceinfo.UsbSettings;
+import com.android.settings.aicp.AmbientSettings;
 import com.android.settings.fuelgauge.BatterySaverSettings;
 import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.notification.NotificationAppList;
@@ -324,7 +325,8 @@ public class SettingsActivity extends Activity
             com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
             QSTiles.class.getName(),
             DisplayRotation.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            AmbientSettings.class.getName()
     };
 
 
--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -177,6 +177,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_power";
                 stringId = R.string.qs_screen_off_tile;
                 break;
+            case QSConstants.TILE_AMBIENT_DISPLAY:
+                resourceName = "ic_qs_doze";
+                stringId = R.string.qs_ambient_display_tile;
+                break;
             default:
                 return null;
         }
