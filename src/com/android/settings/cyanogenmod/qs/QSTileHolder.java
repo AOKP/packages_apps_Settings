b95e743447dc4047c6e0c29a17830036bf5ce77c
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-03-05 19:35:51 +0100

    QSTile: Reboot/Recovery tile (2/2)
    Change-Id: I4b49f915ee8829102489578a75369c48c99fceaf

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -181,6 +181,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_doze";
                 stringId = R.string.qs_ambient_display_tile;
                 break;
+            case QSConstants.TILE_REBOOT:
+                resourceName = "ic_qs_reboot";
+                stringId = R.string.qs_tile_reboot;
+                break;
             default:
                 return null;
         }
