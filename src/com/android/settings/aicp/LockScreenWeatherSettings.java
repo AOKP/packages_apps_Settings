63a668c6a85526717387db6e80d5206203e7466c
XXMrHyde <email@address.com>
2015-03-12 14:50:15 +0100

    Lock screen: Text and icon colors, (2/2)
    Change-Id: Ib4a085aa00769b05530f6dfdfbf18c0792b31cc3

--- src/com/android/settings/aicp/LockScreenWeatherSettings.java
+++ src/com/android/settings/aicp/LockScreenWeatherSettings.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2014 DarkKat
+ * Copyright (C) 2015 DarkKat
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,13 +36,9 @@ import android.view.MenuItem;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
-
 public class LockScreenWeatherSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String PREF_CAT_COLORS =
-            "weather_cat_colors";
     private static final String PREF_SHOW_WEATHER =
             "weather_show_weather";
     private static final String PREF_SHOW_LOCATION =
@@ -53,14 +49,8 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
             "weather_condition_icon";
     private static final String PREF_COLORIZE_ALL_ICONS =
             "weather_colorize_all_icons";
-    private static final String PREF_TEXT_COLOR =
-            "weather_text_color";
-    private static final String PREF_ICON_COLOR =
-            "weather_icon_color";
 
     private static final int MONOCHROME_ICON = 0;
-    private static final int DEFAULT_COLOR = 0xffffffff;
-    private static final int HOLO_BLUE_LIGHT = 0xff33b5e5;
 
     private static final int MENU_RESET = Menu.FIRST;
     private static final int DLG_RESET = 0;
@@ -70,8 +60,6 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
     private CheckBoxPreference mShowTimestamp;
     private ListPreference mConditionIcon;
     private CheckBoxPreference mColorizeAllIcons;
-    private ColorPickerPreference mTextColor;
-    private ColorPickerPreference mIconColor;
 
     private ContentResolver mResolver;
 
@@ -92,25 +80,11 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
 
         boolean showWeather = Settings.System.getInt(mResolver,
                 Settings.System.LOCK_SCREEN_SHOW_WEATHER, 0) == 1;
-        int conditionIcon = Settings.System.getInt(mResolver,
-               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
-        boolean colorizeAllIcons = Settings.System.getInt(mResolver,
-                    Settings.System.LOCK_SCREEN_WEATHER_COLORIZE_ALL_ICONS, 0) == 1;
-
-        int intColor;
-        String hexColor;
 
         mShowWeather =
                 (CheckBoxPreference) findPreference(PREF_SHOW_WEATHER);
         mShowWeather.setChecked(showWeather);
         mShowWeather.setOnPreferenceChangeListener(this);
-
-        PreferenceCategory catColors =
-                (PreferenceCategory) findPreference(PREF_CAT_COLORS);
-        mTextColor =
-                (ColorPickerPreference) findPreference(PREF_TEXT_COLOR);
-        mIconColor =
-                (ColorPickerPreference) findPreference(PREF_ICON_COLOR);
 
         if (showWeather) {
             mShowLocation =
@@ -127,43 +101,22 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
 
             mConditionIcon =
                     (ListPreference) findPreference(PREF_CONDITION_ICON);
+            int conditionIcon = Settings.System.getInt(mResolver,
+                   Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
             mConditionIcon.setValue(String.valueOf(conditionIcon));
             mConditionIcon.setSummary(mConditionIcon.getEntry());
             mConditionIcon.setOnPreferenceChangeListener(this);
 
             mColorizeAllIcons =
                     (CheckBoxPreference) findPreference(PREF_COLORIZE_ALL_ICONS);
-            mColorizeAllIcons.setChecked(colorizeAllIcons);
+            mColorizeAllIcons.setChecked(Settings.System.getInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_COLORIZE_ALL_ICONS, 0) == 1);
             mColorizeAllIcons.setOnPreferenceChangeListener(this);
-
-            intColor = Settings.System.getInt(mResolver,
-                    Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR,
-                    DEFAULT_COLOR); 
-            mTextColor.setNewPreviewColor(intColor);
-            hexColor = String.format("#%08x", (0xffffffff & intColor));
-            mTextColor.setSummary(hexColor);
-            mTextColor.setOnPreferenceChangeListener(this);
         } else {
             removePreference(PREF_SHOW_LOCATION);
             removePreference(PREF_SHOW_TIMESTAMP);
             removePreference(PREF_CONDITION_ICON);
             removePreference(PREF_COLORIZE_ALL_ICONS);
-            catColors.removePreference(mTextColor);
-        }
-        if (showWeather && ((conditionIcon == MONOCHROME_ICON)
-                || (conditionIcon != MONOCHROME_ICON && colorizeAllIcons))) {
-            intColor = Settings.System.getInt(mResolver,
-                    Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR,
-                    DEFAULT_COLOR); 
-            mIconColor.setNewPreviewColor(intColor);
-            hexColor = String.format("#%08x", (0xffffffff & intColor));
-            mIconColor.setSummary(hexColor);
-            mIconColor.setOnPreferenceChangeListener(this);
-        } else {
-            catColors.removePreference(mIconColor);
-            if (!showWeather) {
-                removePreference(PREF_CAT_COLORS);
-            }
         }
 
         setHasOptionsMenu(true);
@@ -189,8 +142,6 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         boolean value;
-        String hex;
-        int intHex;
 
         if (preference == mShowWeather) {
             value = (Boolean) newValue;
@@ -217,30 +168,12 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
             Settings.System.putInt(mResolver,
                     Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
             mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
-            refreshSettings();
             return true;
         } else if (preference == mColorizeAllIcons) {
             value = (Boolean) newValue;
             Settings.System.putInt(mResolver,
                     Settings.System.LOCK_SCREEN_WEATHER_COLORIZE_ALL_ICONS,
                     value ? 1 : 0);
-            refreshSettings();
-            return true;
-        } else if (preference == mTextColor) {
-            hex = ColorPickerPreference.convertToARGB(
-                Integer.valueOf(String.valueOf(newValue)));
-            intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(mResolver,
-                    Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR, intHex);
-            preference.setSummary(hex);
-            return true;
-        } else if (preference == mIconColor) {
-            hex = ColorPickerPreference.convertToARGB(
-                Integer.valueOf(String.valueOf(newValue)));
-            intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(mResolver,
-                    Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR, intHex);
-            preference.setSummary(hex);
             return true;
         }
         return false;
@@ -289,12 +222,6 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
                                     MONOCHROME_ICON);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.LOCK_SCREEN_WEATHER_COLORIZE_ALL_ICONS, 0);
-                            Settings.System.putInt(getOwner().mResolver,
-                                    Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR,
-                                    DEFAULT_COLOR);
-                            Settings.System.putInt(getOwner().mResolver,
-                                    Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR,
-                                    DEFAULT_COLOR);
                             getOwner().refreshSettings();
                         }
                     })
@@ -311,12 +238,6 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
                                     Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, 2);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.LOCK_SCREEN_WEATHER_COLORIZE_ALL_ICONS, 1);
-                            Settings.System.putInt(getOwner().mResolver,
-                                    Settings.System.LOCK_SCREEN_WEATHER_TEXT_COLOR,
-                                    HOLO_BLUE_LIGHT);
-                            Settings.System.putInt(getOwner().mResolver,
-                                    Settings.System.LOCK_SCREEN_WEATHER_ICON_COLOR,
-                                    HOLO_BLUE_LIGHT);
                             getOwner().refreshSettings();
                         }
                     })
