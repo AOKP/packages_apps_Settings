4cfb88a3a683fa05115dab05046f06e3057f2856
alviteri <davidteri91@gmail.com>
2015-03-14 19:47:02 +0100

    Settings: Clean code for HeadsUp
    Change-Id: I2d9b1d30a25b219f0ccdc9f97bbffc0b19b5c1ec

--- src/com/android/settings/aicp/HeadsUpSettings.java
+++ src/com/android/settings/aicp/HeadsUpSettings.java
@@ -44,9 +44,9 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Switch;
 
+import com.android.internal.util.cm.ScreenType;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 import com.android.settings.cyanogenmod.PackageListAdapter;
 import com.android.settings.cyanogenmod.PackageListAdapter.PackageItem;
 import com.android.settings.SettingsActivity;
@@ -170,7 +170,7 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
 
         // If running on a phone, remove padding around container
         // and the preference listview
-        if (!Utils.isTablet(getActivity())) {
+        if (!ScreenType.isTablet(getActivity())) {
             mPrefsContainer.setPadding(0, 0, 0, 0);
             getListView().setPadding(0, 0, 0, 0);
         }
