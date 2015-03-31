7843ca5d9eac34b58f14acebc4a2f12f2883c17c
Jubakuba <Jubakuba@gmail.com>
2015-03-31 22:27:34 +0200

    NFC Polling Mode (2/3)
    Change-Id: I0e17ded93e9b82095e6bf8301c268017c49dd12b
    Signed-off-by: Jubakuba <Jubakuba@gmail.com>

--- src/com/android/settings/WirelessSettings.java
+++ src/com/android/settings/WirelessSettings.java
@@ -37,6 +37,7 @@ import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
@@ -61,7 +62,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class WirelessSettings extends SettingsPreferenceFragment
-        implements OnPreferenceChangeListener, Indexable {
+        implements Preference.OnPreferenceChangeListener, Indexable {
     private static final String TAG = "WirelessSettings";
 
     private static final String KEY_TOGGLE_AIRPLANE = "toggle_airplane";
@@ -76,6 +77,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
     private static final String KEY_SMS_APPLICATION = "sms_application";
     private static final String KEY_TOGGLE_NSD = "toggle_nsd"; //network service discovery
     private static final String KEY_CELL_BROADCAST_SETTINGS = "cell_broadcast_settings";
+    private static final String KEY_NFC_POLLING_MODE = "nfc_polling_mode";
 
     public static final String EXIT_ECM_RESULT = "exit_ecm_result";
     public static final int REQUEST_CODE_EXIT_ECM = 1;
@@ -85,6 +87,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
     private NfcEnabler mNfcEnabler;
     private NfcAdapter mNfcAdapter;
     private NsdEnabler mNsdEnabler;
+    private ListPreference mNfcPollingMode;
 
     private ConnectivityManager mCm;
     private TelephonyManager mTm;
@@ -287,8 +290,14 @@ public class WirelessSettings extends SettingsPreferenceFragment
         PreferenceScreen androidBeam = (PreferenceScreen) findPreference(KEY_ANDROID_BEAM_SETTINGS);
         SwitchPreference nsd = (SwitchPreference) findPreference(KEY_TOGGLE_NSD);
 
+        mNfcPollingMode = (ListPreference) findPreference(KEY_NFC_POLLING_MODE);
+        mNfcPollingMode.setOnPreferenceChangeListener(this);
+        mNfcPollingMode.setValue((Settings.System.getInt(activity.getContentResolver(),
+                Settings.System.NFC_POLLING_MODE, 3)) + "");
+        updateNfcPolling();
+
         mAirplaneModeEnabler = new AirplaneModeEnabler(activity, mAirplaneModePreference);
-        mNfcEnabler = new NfcEnabler(activity, nfc, androidBeam);
+        mNfcEnabler = new NfcEnabler(activity, nfc, androidBeam, mNfcPollingMode);
 
         mSmsApplicationPreference = (AppListPreference) findPreference(KEY_SMS_APPLICATION);
         // Restricted users cannot currently read/write SMS.
@@ -346,6 +355,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
         mNfcAdapter = NfcAdapter.getDefaultAdapter(activity);
         if (mNfcAdapter == null) {
             getPreferenceScreen().removePreference(nfc);
+            getPreferenceScreen().removePreference(mNfcPollingMode);
             getPreferenceScreen().removePreference(androidBeam);
             mNfcEnabler = null;
         }
@@ -429,6 +439,25 @@ public class WirelessSettings extends SettingsPreferenceFragment
         initSmsApplicationSetting();
     }
 
+    private void updateNfcPolling() {
+        int resId;
+        String value = Settings.System.getString(getContentResolver(),
+                Settings.System.NFC_POLLING_MODE);
+        String[] pollingArray = getResources().getStringArray(R.array.nfc_polling_mode_values);
+
+        if (pollingArray[0].equals(value)) {
+            resId = R.string.nfc_polling_mode_screen_off;
+            mNfcPollingMode.setValueIndex(0);
+        } else if (pollingArray[1].equals(value)) {
+            resId = R.string.nfc_polling_mode_screen_locked;
+            mNfcPollingMode.setValueIndex(1);
+        } else {
+            resId = R.string.nfc_polling_mode_screen_unlocked;
+            mNfcPollingMode.setValueIndex(2);
+        }
+        mNfcPollingMode.setSummary(getResources().getString(resId));
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -484,6 +513,12 @@ public class WirelessSettings extends SettingsPreferenceFragment
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mSmsApplicationPreference && newValue != null) {
             SmsApplication.setDefaultApplication(newValue.toString(), getActivity());
+            return true;
+        } else if (preference == mNfcPollingMode) {
+            int newVal = Integer.parseInt((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NFC_POLLING_MODE, newVal);
+            updateNfcPolling();
             return true;
         }
         return false;
--- src/com/android/settings/nfc/NfcEnabler.java
+++ src/com/android/settings/nfc/NfcEnabler.java
@@ -23,6 +23,7 @@ import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.os.UserManager;
 import android.preference.Preference;
+import android.preference.ListPreference;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 
@@ -37,6 +38,7 @@ public class NfcEnabler implements Preference.OnPreferenceChangeListener {
     private final Context mContext;
     private final SwitchPreference mSwitch;
     private final PreferenceScreen mAndroidBeam;
+    private final ListPreference mNfcPollingMode;
     private final NfcAdapter mNfcAdapter;
     private final IntentFilter mIntentFilter;
     private boolean mBeamDisallowed;
@@ -53,10 +55,11 @@ public class NfcEnabler implements Preference.OnPreferenceChangeListener {
     };
 
     public NfcEnabler(Context context, SwitchPreference switchPreference,
-            PreferenceScreen androidBeam) {
+            PreferenceScreen androidBeam, ListPreference nfcPollingMode) {
         mContext = context;
         mSwitch = switchPreference;
         mAndroidBeam = androidBeam;
+        mNfcPollingMode = nfcPollingMode;
         mNfcAdapter = NfcAdapter.getDefaultAdapter(context);
         mBeamDisallowed = ((UserManager) mContext.getSystemService(Context.USER_SERVICE))
                 .hasUserRestriction(UserManager.DISALLOW_OUTGOING_BEAM);
@@ -65,6 +68,7 @@ public class NfcEnabler implements Preference.OnPreferenceChangeListener {
             // NFC is not supported
             mSwitch.setEnabled(false);
             mAndroidBeam.setEnabled(false);
+            mNfcPollingMode.setEnabled(false);
             mIntentFilter = null;
             return;
         }
@@ -113,11 +117,13 @@ public class NfcEnabler implements Preference.OnPreferenceChangeListener {
             mSwitch.setEnabled(true);
             mAndroidBeam.setEnabled(false);
             mAndroidBeam.setSummary(R.string.android_beam_disabled_summary);
+            mNfcPollingMode.setEnabled(false);
             break;
         case NfcAdapter.STATE_ON:
             mSwitch.setChecked(true);
             mSwitch.setEnabled(true);
             mAndroidBeam.setEnabled(!mBeamDisallowed);
+            mNfcPollingMode.setEnabled(true);
             if (mNfcAdapter.isNdefPushEnabled() && !mBeamDisallowed) {
                 mAndroidBeam.setSummary(R.string.android_beam_on_summary);
             } else {
@@ -128,11 +134,13 @@ public class NfcEnabler implements Preference.OnPreferenceChangeListener {
             mSwitch.setChecked(true);
             mSwitch.setEnabled(false);
             mAndroidBeam.setEnabled(false);
+            mNfcPollingMode.setEnabled(false);
             break;
         case NfcAdapter.STATE_TURNING_OFF:
             mSwitch.setChecked(false);
             mSwitch.setEnabled(false);
             mAndroidBeam.setEnabled(false);
+            mNfcPollingMode.setEnabled(false);
             break;
         }
     }
