08d8930b349320a3bc04fe2984a0b4a9c80e0f86
camcory <camcory1@gmail.com>
2015-04-15 16:31:47 +0200

    Settings: Add music tile (2/2)
    Change-Id: Ie36994028ce5c827e31cbc9d17161be5eee2894f

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -193,6 +193,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_reboot_recovery";
                 stringId = R.string.qs_tile_slimaction;
                 break;
+            case QSConstants.TILE_MUSIC:
+                resourceName = "ic_qs_media_play";
+                stringId = R.string.qs_music_play_tile;
+                break;
             default:
                 return null;
         }
