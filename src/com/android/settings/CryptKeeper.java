6364a537d45ebac258d27b5618c92d6dab544cf5
Roman Birg <roman@cyngn.com>
2015-05-01 17:07:47 -0700

    Settings: fix non lock pattern CryptKeeper crash
    Change-Id: Ib2d6c9a468bfe778d5cb927759f11b2b03c25ee6
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/CryptKeeper.java
+++ src/com/android/settings/CryptKeeper.java
@@ -189,7 +189,9 @@ public class CryptKeeper extends Activity implements TextView.OnEditorActionList
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            mLockPatternView.removeCallbacks(mFakeUnlockAttemptRunnable);
+            if (mLockPatternView != null) {
+                mLockPatternView.removeCallbacks(mFakeUnlockAttemptRunnable);
+            }
             beginAttempt();
         }
 
