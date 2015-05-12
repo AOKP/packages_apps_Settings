ae16ec4285b3e0b10d606825dc9ee47930e37e0f
Griffin Millender <griffinn.millender@gmail.com>
2015-05-12 16:33:35 -0500

    Add double tap option to lockscreen shortcuts
    Change-Id: I640a8724e41b5549c0b949b2e94b54a0c5834e4b

--- src/com/android/settings/slim/fragments/LockscreenShortcutFragment.java
+++ src/com/android/settings/slim/fragments/LockscreenShortcutFragment.java
@@ -20,7 +20,7 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.preference.ListPreference;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,10 +33,10 @@ import com.android.settings.SettingsPreferenceFragment;
 public class LockscreenShortcutFragment extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-    private static final String PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS =
-            "lockscreen_shortcuts_longpress";
+    private static final String PREF_LOCKSCREEN_SHORTCUTS_LAUNCH_TYPE =
+            "lockscreen_shortcuts_launch_type";
 
-    private SwitchPreference mLockscreenShortcutsLongpress;
+    private ListPreference mLockscreenShortcutsLaunchType;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,11 +46,9 @@ public class LockscreenShortcutFragment extends SettingsPreferenceFragment imple
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
-        mLockscreenShortcutsLongpress = (SwitchPreference) findPreference(
-                PREF_LOCKSCREEN_SHORTCUTS_LONGPRESS);
-        mLockscreenShortcutsLongpress.setChecked(Settings.System.getInt(getContentResolver(),
-                Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS, 1) == 1);
-        mLockscreenShortcutsLongpress.setOnPreferenceChangeListener(this);
+        mLockscreenShortcutsLaunchType = (ListPreference) findPreference(
+                PREF_LOCKSCREEN_SHORTCUTS_LAUNCH_TYPE);
+        mLockscreenShortcutsLaunchType.setOnPreferenceChangeListener(this);
 
         setHasOptionsMenu(false);
     }
@@ -71,10 +69,10 @@ public class LockscreenShortcutFragment extends SettingsPreferenceFragment imple
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mLockscreenShortcutsLongpress) {
+        if (preference == mLockscreenShortcutsLaunchType) {
             Settings.System.putInt(getContentResolver(),
                     Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS,
-                    (Boolean) newValue ? 1 : 0);
+                    Integer.valueOf((String) newValue));
         }
         return true;
     }
