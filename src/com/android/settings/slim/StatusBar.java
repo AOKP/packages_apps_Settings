35b00f5fe8ac33dda9e3639ae4876dfd64e8d444
LorDClockaN <davor@losinj.com>
2015-05-26 15:28:01 +0200

    Revert "Show 4G instead of LTE switch [2/2]"
    This reverts commit 2f37eba533f1f16469be618b69a4a1b049859282.
    
    Change-Id: I2fb93a46bcb37f20e8df7cf09def0ce2f50aead7

--- src/com/android/settings/slim/StatusBar.java
+++ src/com/android/settings/slim/StatusBar.java
@@ -41,7 +41,6 @@ import android.widget.EditText;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-import com.android.settings.util.Helpers;
 import com.android.settings.widget.SeekBarPreferenceCham;
 
 import com.android.internal.util.slim.DeviceUtils;
@@ -58,7 +57,6 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private static final String KEY_STATUS_BAR_GREETING = "status_bar_greeting";
     private static final String KEY_STATUS_BAR_GREETING_TIMEOUT = "status_bar_greeting_timeout";
     private static final String KEY_AICP_LOGO_COLOR = "status_bar_aicp_logo_color";
-    private static final String SHOW_FOURG = "show_fourg";
 
     private SwitchPreference mStatusBarBrightnessControl;
     private PreferenceScreen mCarrierLabel;
@@ -67,7 +65,6 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private SwitchPreference mStatusBarGreeting;
     private SeekBarPreferenceCham mStatusBarGreetingTimeout;
     private ColorPickerPreference mAicpLogoColor;
-    private SwitchPreference mShowFourG;
 
     private String mCustomGreetingText = "";
 
@@ -139,14 +136,6 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
             mAicpLogoColor.setSummary(hexColor);
             mAicpLogoColor.setNewPreviewColor(intColor);
 
-        //  4G LTE switch
-        mShowFourG = (SwitchPreference) prefSet.findPreference(SHOW_FOURG);
-        if (Utils.isWifiOnly(getActivity())) {
-            prefSet.removePreference(mShowFourG);
-        } else {
-            mShowFourG.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.SHOW_FOURG, 0) == 1));
-        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -214,12 +203,6 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
                 Settings.System.putString(getActivity().getContentResolver(),
                         Settings.System.STATUS_BAR_GREETING, "");
             }
-        } else if  (preference == mShowFourG) {
-            boolean enabled = ((SwitchPreference)preference).isChecked();
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.SHOW_FOURG, enabled ? 1:0);
-            Helpers.restartSystemUI();
-            return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
