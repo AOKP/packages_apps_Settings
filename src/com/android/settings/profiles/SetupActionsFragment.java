397fadeefd701e20eaef5d6df02a1d5b637e6d51
Raj Yengisetty <rajesh@cyngn.com>
2015-05-29 10:17:29 -0700

    Profiles: fix SeekBarVolumizer behavior
    - Seekbar needs to be setEnabled() *after* setting the seekbar to
    the volumizer becase volumizer.setSeekBar() will set the seekbar's
    enabled state.
    
    Change-Id: Ia5b3b8cfaebb54997a60a7f09915e1f22016c060

--- src/com/android/settings/profiles/SetupActionsFragment.java
+++ src/com/android/settings/profiles/SetupActionsFragment.java
@@ -772,10 +772,12 @@ public class SetupActionsFragment extends SettingsPreferenceFragment
                 seekBar.setEnabled(isChecked);
             }
         });
-        seekBar.setEnabled(streamSettings.isOverride());
         final SeekBarVolumizer volumizer = new SeekBarVolumizer(getActivity(), streamId, null,
                 null);
+        volumizer.start();
         volumizer.setSeekBar(seekBar);
+        seekBar.setEnabled(streamSettings.isOverride());
+
         builder.setView(view);
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
