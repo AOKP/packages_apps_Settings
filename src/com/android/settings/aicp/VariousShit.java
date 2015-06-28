2c0656e1be649a61391725c3edc2e8bc498fc9b0
Alex Cruz <mazdarider23@gmail.com>
2015-06-28 21:11:50 +0200

    Selinux Switch
    DU PS21
    
    PS3: update VariousSh*t and OnBoot logic with DU PS9
    PS4: fix mess
    PS5: Resolve conflicts after Floating Windows change
    PS5: rebase
    
    Change-Id: I84f64f09c87806f2382abe746493f33b0b209de0

--- src/com/android/settings/aicp/VariousShit.java
+++ src/com/android/settings/aicp/VariousShit.java
@@ -67,6 +67,7 @@ import com.android.settings.util.Helpers;
 import com.android.settings.Utils;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -112,6 +113,8 @@ public class VariousShit extends SettingsPreferenceFragment
     private static final String DISABLE_TORCH_ON_SCREEN_OFF = "disable_torch_on_screen_off";
     private static final String DISABLE_TORCH_ON_SCREEN_OFF_DELAY = "disable_torch_on_screen_off_delay";
 
+    private static final String SELINUX = "selinux";
+
     // Package name of the yoga
     public static final String YOGA_PACKAGE_NAME = "com.android.settings";
     // Intent for launching the yoga actvity
@@ -138,6 +141,7 @@ public class VariousShit extends SettingsPreferenceFragment
     private ListPreference mTorchOffDelay;
     private PreferenceCategory mTorchCategory;
     private Preference mLockClock;
+    private SwitchPreference mSelinux;
 
     private Preference mHiddenShit;
     private PreferenceScreen mHiddenImg;
@@ -218,6 +222,17 @@ public class VariousShit extends SettingsPreferenceFragment
             prefSet.removePreference(mTorchCategory);
         }
 
+        //SELinux
+        mSelinux = (SwitchPreference) findPreference(SELINUX);
+        mSelinux.setOnPreferenceChangeListener(this);
+
+        if (CMDProcessor.runSuCommand("getenforce").getStdout().contains("Enforcing")) {
+            mSelinux.setChecked(true);
+            mSelinux.setSummary(R.string.selinux_enforcing_title);
+        } else {
+            mSelinux.setChecked(false);
+            mSelinux.setSummary(R.string.selinux_permissive_title);
+        }
     }
 
     @Override
@@ -284,6 +299,15 @@ public class VariousShit extends SettingsPreferenceFragment
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.DISABLE_TORCH_ON_SCREEN_OFF_DELAY, torchOffDelay);
             mTorchOffDelay.setSummary(mTorchOffDelay.getEntries()[index]);
+            return true;
+        } else if (preference == mSelinux) {
+            if (objValue.toString().equals("true")) {
+                CMDProcessor.runSuCommand("setenforce 1");
+                mSelinux.setSummary(R.string.selinux_enforcing_title);
+            } else if (objValue.toString().equals("false")) {
+                CMDProcessor.runSuCommand("setenforce 0");
+                mSelinux.setSummary(R.string.selinux_permissive_title);
+            }
             return true;
         }
         return false;
@@ -567,5 +591,5 @@ public class VariousShit extends SettingsPreferenceFragment
                 "chmod 644 /system/media/bootanimation.zip",
                 "mount -o ro,remount /system");
     }
-
 }
+
