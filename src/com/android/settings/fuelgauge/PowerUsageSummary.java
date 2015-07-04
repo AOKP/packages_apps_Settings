57e26fa81937851eac6a9c234d82483918ef3810
Jorge Ruesga <jorge@ruesga.com>
2015-07-04 18:05:16 +0200

    settings: dock battery
    Change-Id: I2a95e74719175c3f40e2cc7a8414db7e869d92fb
    Require: topic:dock_battery
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -22,6 +22,7 @@ import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
@@ -81,11 +82,13 @@ public class PowerUsageSummary extends SettingsPreferenceFragment
     private static final String KEY_PER_APP_PROFILES = "app_perf_profiles_enabled";
 
     private static final String BATTERY_HISTORY_FILE = "tmp_bat_history.bin";
+    private static final String DOCK_BATTERY_HISTORY_FILE = "tmp_dock_bat_history.bin";
 
     private static final int MENU_STATS_TYPE = Menu.FIRST;
     private static final int MENU_STATS_REFRESH = Menu.FIRST + 1;
-    private static final int MENU_BATTERY_SAVER = Menu.FIRST + 2;
-    private static final int MENU_HELP = Menu.FIRST + 3;
+    private static final int MENU_STATS_RESET = Menu.FIRST + 2;
+    private static final int MENU_BATTERY_SAVER = Menu.FIRST + 3;
+    private static final int MENU_HELP = Menu.FIRST + 4;
 
     private UserManager mUm;
 
@@ -104,6 +107,7 @@ public class PowerUsageSummary extends SettingsPreferenceFragment
 
     private BatteryStatsHelper mStatsHelper;
 
+    private BatteryManager mBatteryManager;
     private PowerManager mPowerManager;
     private ListPreference mPerfProfilePref;
     private SwitchPreference mBatterySaverPref;
@@ -150,6 +154,8 @@ public class PowerUsageSummary extends SettingsPreferenceFragment
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         mStatsHelper.create(icicle);
+
+        mBatteryManager = (BatteryManager) getSystemService(Context.BATTERY_SERVICE);
 
         mPerfProfileEntries = getResources().getStringArray(
                 com.android.internal.R.array.perf_profile_entries);
@@ -245,6 +251,10 @@ public class PowerUsageSummary extends SettingsPreferenceFragment
             mStatsHelper.storeStatsHistoryInFile(BATTERY_HISTORY_FILE);
             Bundle args = new Bundle();
             args.putString(BatteryHistoryDetail.EXTRA_STATS, BATTERY_HISTORY_FILE);
+            if (mBatteryManager.isDockBatterySupported()) {
+                mStatsHelper.storeDockStatsHistoryInFile(DOCK_BATTERY_HISTORY_FILE);
+                args.putString(BatteryHistoryDetail.EXTRA_DOCK_STATS, DOCK_BATTERY_HISTORY_FILE);
+            }
             args.putParcelable(BatteryHistoryDetail.EXTRA_BROADCAST,
                     mStatsHelper.getBatteryBroadcast());
             SettingsActivity sa = (SettingsActivity) getActivity();
@@ -286,6 +296,11 @@ public class PowerUsageSummary extends SettingsPreferenceFragment
                 .setAlphabeticShortcut('r');
         refresh.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |
                 MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+        MenuItem reset = menu.add(0, MENU_STATS_RESET, 0, R.string.menu_stats_reset)
+                .setIcon(R.drawable.ic_actionbar_delete)
+                .setAlphabeticShortcut('d');
+        reset.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |
+                MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 
         MenuItem batterySaver = menu.add(0, MENU_BATTERY_SAVER, 0,
                 R.string.battery_saver_threshold);
@@ -308,6 +323,9 @@ public class PowerUsageSummary extends SettingsPreferenceFragment
                     mStatsType = BatteryStats.STATS_SINCE_CHARGED;
                 }
                 refreshStats();
+                return true;
+            case MENU_STATS_RESET:
+                resetStats();
                 return true;
             case MENU_STATS_REFRESH:
                 mStatsHelper.clearStats();
@@ -357,6 +375,24 @@ public class PowerUsageSummary extends SettingsPreferenceFragment
             default:
                 return false;
         }
+    }
+
+    private void resetStats() {
+        AlertDialog dialog = new AlertDialog.Builder(getActivity())
+            .setTitle(R.string.menu_stats_reset)
+            .setMessage(R.string.reset_stats_msg)
+            .setPositiveButton(android.R.string.ok, new OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Reset stats and request a refresh to initialize vars
+                    mStatsHelper.resetStatistics();
+                    refreshStats();
+                    mHandler.removeMessages(MSG_REFRESH_STATS);
+                }
+            })
+            .setNegativeButton(android.R.string.cancel, null)
+            .create();
+        dialog.show();
     }
 
     private void refreshBatterySaverOptions() {
@@ -423,9 +459,12 @@ public class PowerUsageSummary extends SettingsPreferenceFragment
     }
 
     private void refreshStats() {
+        final BatteryStats stats = mStatsHelper.getStats();
+        final BatteryStats dockStats = mStatsHelper.getDockStats();
+
         mAppListGroup.removeAll();
         mAppListGroup.setOrderingAsAdded(false);
-        mHistPref = new BatteryHistoryPreference(getActivity(), mStatsHelper.getStats(),
+        mHistPref = new BatteryHistoryPreference(getActivity(), stats, dockStats,
                 mStatsHelper.getBatteryBroadcast());
         mHistPref.setOrder(-1);
         mAppListGroup.addPreference(mHistPref);
@@ -436,7 +475,6 @@ public class PowerUsageSummary extends SettingsPreferenceFragment
                 DevelopmentSettings.SHOW_UNAC_AND_OVERCOUNTED_STATS, false);
 
         final PowerProfile powerProfile = mStatsHelper.getPowerProfile();
-        final BatteryStats stats = mStatsHelper.getStats();
         final double averagePower = powerProfile.getAveragePower(PowerProfile.POWER_SCREEN_FULL);
         if (averagePower >= MIN_AVERAGE_POWER_THRESHOLD_MILLI_AMP) {
             final List<UserHandle> profiles = mUm.getUserProfiles();
