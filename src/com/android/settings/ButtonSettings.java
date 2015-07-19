e4d7c8d94ecbf21e040f79dfd0ee54c2d1c776ba
LorDClockaN <davor@losinj.com>
2015-07-19 06:55:11 +0200

    Revert "Add option to set default custom function to button recents (2/2)"
    This reverts commit 280e4f4c10791df95c06e40a87f320e9362b1663.
    
    Change-Id: Id7c7ec31c707f57eb515fe20b1b2799a0ff4fa24

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -405,8 +405,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                         KEY_APP_SWITCH_PRESS, pressAction);
 
                 int longPressAction = Settings.System.getInt(resolver,
-                        Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION,
-                        hasMenuKey ? ACTION_NOTHING : ACTION_MENU);
+                        Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION, ACTION_NOTHING);
                 settings.mAppSwitchLongPressAction = settings.initActionList(
                         KEY_APP_SWITCH_LONG_PRESS, longPressAction);
             }
