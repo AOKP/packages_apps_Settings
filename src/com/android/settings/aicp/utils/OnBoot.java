49ec782a0c103dd68ebe4e7c7d89d1442cd0b342
maxwen <max.weninger@gmail.com>
2015-09-04 16:28:16 +0200

    Simplify OnBoot for selinux switch
    Change-Id: I4cc114171f75348155bc0b40893c322afc519fb9

--- src/com/android/settings/aicp/utils/OnBoot.java
+++ src/com/android/settings/aicp/utils/OnBoot.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -30,12 +31,7 @@ public class OnBoot extends BroadcastReceiver {
             }
         }
         if(!mSetupRunning) {
-             try {
-                 settingsContext = context.createPackageContext("com.android.settings", 0);
-             } catch (Exception e) {
-                 Log.e(TAG, "Package not found", e);
-             }
-             SharedPreferences sharedpreferences = settingsContext.getSharedPreferences("com.android.settings_preferences", Context.MODE_PRIVATE);
+             SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(context);
              if(sharedpreferences.getBoolean("selinux", true)) {
                  CMDProcessor.runSuCommand("setenforce 1");
              } else if (!sharedpreferences.getBoolean("selinux", true)) {
