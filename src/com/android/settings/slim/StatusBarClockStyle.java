12cd5a540b476bd58b964650e70f7f2a10015167
Janson Kang <temasek71@gmail.com>
2015-09-06 14:48:15 +0200

    Settings: Make all 14 font variants available to Statusbar Clock (2/2)
    Change-Id: I56fe86146335eac73153886a355ef3cf92ac6106
    Signed-off-by: Janson Kang <temasek71@gmail.com>

--- src/com/android/settings/slim/StatusBarClockStyle.java
+++ src/com/android/settings/slim/StatusBarClockStyle.java
@@ -183,7 +183,7 @@ public class StatusBarClockStyle extends SettingsPreferenceFragment
         mFontStyle.setOnPreferenceChangeListener(this);
         mFontStyle.setValue(Integer.toString(Settings.System.getInt(
                 getActivity().getContentResolver(),
-                Settings.System.STATUSBAR_CLOCK_FONT_STYLE, 4)));
+                Settings.System.STATUSBAR_CLOCK_FONT_STYLE, 0)));
         mFontStyle.setSummary(mFontStyle.getEntry());
 
         setHasOptionsMenu(true);
