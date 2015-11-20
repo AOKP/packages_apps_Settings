efa31c2f470b721dcdf97a106923405c5a255b4a
Adam Fisch <adammfisch@gmail.com>
2015-11-20 11:40:45 +0100

    Settings - Add ROM Control quick link back into Settings
    Ported to N thanks to LorDClockaN && camcory
    
    Change-Id: Iac7909b7ac06946f8aabba14498f9cc5ef683c51
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -161,4 +161,5 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
+    public static class ROMControlActivity extends SettingsActivity { /* empty */ }
 }
