cb31b6615f8d5b530131c478771eeec9219849fa
Lars Greiss <kufikugel@googlemail.com>
2015-12-18 23:13:54 +0100

    Settings: add user configurable lockscreen shortcuts (2/2)
    AICPfy
    - Bring from AICP LP5.0 with a little help from DU
    - It includes this: http://gerrit.aicp-rom.com/#/c/11877/1
    
    Change-Id: I6333792830f7357a9f4fcd5d42c479ec60a71915

---
+++ src/com/android/settings/aicp/LockscreenShortcutFragment.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aicp;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.ListPreference;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.MetricsLogger;
+
+public class LockscreenShortcutFragment extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String PREF_LOCKSCREEN_SHORTCUTS_LAUNCH_TYPE =
+            "lockscreen_shortcuts_launch_type";
+
+    private ListPreference mLockscreenShortcutsLaunchType;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen_shortcut_fragment);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLockscreenShortcutsLaunchType = (ListPreference) findPreference(
+                PREF_LOCKSCREEN_SHORTCUTS_LAUNCH_TYPE);
+        mLockscreenShortcutsLaunchType.setOnPreferenceChangeListener(this);
+
+        setHasOptionsMenu(false);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DONT_TRACK_ME_BRO;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        if (list != null) {
+            int paddingTop = list.getPaddingTop();
+            int paddingBottom = list.getPaddingBottom();
+            list.setPadding(0, paddingTop, 0, paddingBottom);
+        }
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockscreenShortcutsLaunchType) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_SHORTCUTS_LONGPRESS,
+                    Integer.valueOf((String) newValue));
+        }
+        return true;
+    }
+}
