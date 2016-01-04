6108939b5e5dfb9ff67764c8ea426f8858ea27b6
Alex Cruz <mazdarider23@gmail.com>
2016-01-04 16:53:46 +0200

    settings: Change default Animation scale value to 0.5
    Change-Id: I22a54bb400e77c111853eb58b2f952a473155fb9

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1629,7 +1629,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private void writeAnimationScaleOption(int which, AnimationScalePreference pref,
             Object newValue) {
         try {
-            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : 1;
+            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : 0.5f;
             mWindowManager.setAnimationScale(which, scale);
             updateAnimationScaleValue(which, pref);
         } catch (RemoteException e) {
