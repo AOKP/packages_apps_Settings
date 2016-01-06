51f99a09fa4c8b158b7cd518efd6578f82e21392
LorDClockaN <davor@losinj.com>
2016-01-06 14:35:59 +0100

    Add AicpSettingsExternal that will be launched from AE
    Change-Id: I057dec33f640a70e142d6fcf46b604c08cc45c6e

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -71,6 +71,7 @@ import com.android.settings.accessibility.AccessibilitySettings;
 import com.android.settings.accessibility.CaptionPropertiesFragment;
 import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
+import com.android.settings.aicp.AicpSettingsExternal;
 import com.android.settings.applications.DrawOverlayDetails;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
@@ -374,7 +375,8 @@ public class SettingsActivity extends Activity
             com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
             BlacklistSettings.class.getName(),
             ProfilesSettings.class.getName(),
-            NotificationManagerSettings.class.getName()
+            NotificationManagerSettings.class.getName(),
+            AicpSettingsExternal.class.getName()
     };
 
 
