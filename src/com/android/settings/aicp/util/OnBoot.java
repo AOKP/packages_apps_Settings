b2c4c517b7a939506d8dc3d9871f496dd89560e9
Alex Cruz <mazdarider23@gmail.com>
2016-01-27 17:57:55 +0100

    Selinux Switch
    Change-Id: Idd38f844f94bdc81e2b690682c73e468156ea10b

---
+++ src/com/android/settings/aicp/util/OnBoot.java
@@ -0,0 +1,51 @@
+package com.android.settings.aicp.util;
+
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.settings.util.CMDProcessor;
+
+import java.io.IOException;
+import java.util.List;
+
+public class OnBoot extends BroadcastReceiver {
+
+    Context settingsContext = null;
+    private static final String TAG = "SettingsOnBoot";
+    Boolean mSetupRunning = false;
+
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+        for(int i = 0; i < procInfos.size(); i++)
+        {
+            if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
+                mSetupRunning = true;
+            }
+        }
+        if(!mSetupRunning) {
+             try {
+                 settingsContext = context.createPackageContext("com.android.settings", 0);
+             } catch (Exception e) {
+                 Log.e(TAG, "Package not found", e);
+             }
+             SharedPreferences sharedpreferences = settingsContext.getSharedPreferences("com.android.settings_preferences", Context.MODE_PRIVATE);
+             if(sharedpreferences.getBoolean("selinux", true)) {
+                 CMDProcessor.runSuCommand("setenforce 1");
+             } else if (!sharedpreferences.getBoolean("selinux", true)) {
+                 CMDProcessor.runSuCommand("setenforce 0");
+             }
+        }
+    }
+}
