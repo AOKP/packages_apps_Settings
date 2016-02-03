f93c6ef72e90068d43b9a424c749b0ff9784f3e0
LorDClockaN <davor@losinj.com>
2016-02-03 22:42:51 +0100

    Fix build
    Change-Id: I95778ff659c81e5b13a195cf9f0d83b7ae9b4152

--- src/com/android/settings/bluetooth/BluetoothSettings.java
+++ src/com/android/settings/bluetooth/BluetoothSettings.java
@@ -61,6 +61,8 @@ import com.android.settingslib.bluetooth.BluetoothDeviceFilter;
 import com.android.settingslib.bluetooth.CachedBluetoothDevice;
 import com.android.settingslib.bluetooth.LocalBluetoothManager;
 
+import cyanogenmod.providers.CMSettings;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
