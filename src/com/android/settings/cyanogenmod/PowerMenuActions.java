38da3fd2125b86573f08d8c9145475152742ae7e
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-05 00:47:05 +0200

    Remove leftover profiles entry from power menu
    * No longer available
    
    Change-Id: Ifac7e1181dc53af5788f54444ef75a345a888fa2

--- src/com/android/settings/cyanogenmod/PowerMenuActions.java
+++ src/com/android/settings/cyanogenmod/PowerMenuActions.java
@@ -37,7 +37,9 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.util.cm.PowerMenuConstants;
+
 import cyanogenmod.providers.CMSettings;
+
 import org.cyanogenmod.internal.logging.CMMetricsLogger;
 
 import static com.android.internal.util.cm.PowerMenuConstants.*;
@@ -57,7 +59,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment
     private CheckBoxPreference mScreenrecordPref;
     private CheckBoxPreference mOnTheGoPref;
     private SlimSeekBarPreference mOnTheGoAlphaPref;
-    private CheckBoxPreference mProfilePref;
     private CheckBoxPreference mAirplanePref;
     private CheckBoxPreference mUsersPref;
     private CheckBoxPreference mSettingsPref;
@@ -329,17 +330,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment
     private void updatePreferences() {
         boolean bugreport = Settings.Secure.getInt(getContentResolver(),
                 Settings.Secure.BUGREPORT_IN_POWER_MENU, 0) != 0;
-        boolean profiles = CMSettings.System.getInt(getContentResolver(),
-                CMSettings.System.SYSTEM_PROFILES_ENABLED, 1) != 0;
-
-        if (mProfilePref != null) {
-            mProfilePref.setEnabled(profiles);
-            if (profiles) {
-                mProfilePref.setSummary(null);
-            } else {
-                mProfilePref.setSummary(R.string.power_menu_profiles_disabled);
-            }
-        }
 
         if (mBugReportPref != null) {
             mBugReportPref.setEnabled(bugreport);
