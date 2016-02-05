cfe8a32061ed37e5b8873382f62001a9e3ba359f
Diogo Ferreira <diogo@underdev.org>
2016-02-05 15:45:05 +0000

    Settings: Add CM-specific settings
    Change-Id: Iae0f60a62a51e96893766c4fa50a60ac4a214102
    Ticket: CYNGNOS-1577

--- src/com/android/settings/search/Ranking.java
+++ src/com/android/settings/search/Ranking.java
@@ -55,6 +55,9 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 
+import com.android.settings.ButtonSettings;
+import com.android.settings.cyanogenmod.StatusBarSettings;
+
 import java.util.HashMap;
 
 /**
@@ -121,6 +124,8 @@ public final class Ranking {
 
         // Display
         sRankMap.put(DisplaySettings.class.getName(), RANK_DISPLAY);
+        sRankMap.put(ButtonSettings.class.getName(), RANK_DISPLAY);
+        sRankMap.put(StatusBarSettings.class.getName(), RANK_DISPLAY);
 
         // Wallpapers
         sRankMap.put(WallpaperTypeSettings.class.getName(), RANK_WALLPAPER);
@@ -160,6 +165,7 @@ public final class Ranking {
 
         // Privacy
         sRankMap.put(PrivacySettings.class.getName(), RANK_PRIVACY);
+        sRankMap.put(com.android.settings.cyanogenmod.PrivacySettings.class.getName(), RANK_PRIVACY);
 
         // Date / Time
         sRankMap.put(DateTimeSettings.class.getName(), RANK_DATE_TIME);
