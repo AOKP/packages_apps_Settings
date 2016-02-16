bc7d8ffbcce8e6710f0c1510d5faa89cead75494
Scott Mertz <scott@cyngn.com>
2016-02-16 09:11:23 -0800

    DevelopmentSettings: initialize recovery update property at boot
    Android behavior has changed to load & trigger build.prop & default.prop
    at every boot.  Once data is mounted, it reloads & triggers properties
    set by the user.  If we set persist.sys.recovery_update to true in build.prop
    or default.prop, the user's preference will never be honored.  Instead,
    ensure the default always comes from userdata.
    
    Accompanying patches to remove the default properties are required.
    
    OPO-490
    Change-Id: I3a5245088059f083554063ba6d345783114fa7a0

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -26,6 +26,7 @@ import com.android.settings.hardware.VibratorIntensity;
 import com.android.settings.inputmethod.InputMethodAndLanguageSettings;
 import com.android.settings.livedisplay.DisplayGamma;
 import com.android.settings.location.LocationSettings;
+import com.android.settings.DevelopmentSettings;
 
 public class BootReceiver extends BroadcastReceiver {
 
@@ -41,5 +42,7 @@ public class BootReceiver extends BroadcastReceiver {
 
         // Extract the contributors database
         ContributorsCloudFragment.extractContributorsCloudDatabase(ctx);
+
+        DevelopmentSettings.initializeUpdateRecoveryOption();
     }
 }
