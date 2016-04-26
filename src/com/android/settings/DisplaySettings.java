16bfb72a7ed384e96d03843462dbaa53b6fd40e8
Steve Kondik <steve@cyngn.com>
2016-04-25 22:46:17 -0700

    settings: Remove LiveDisplay preferences if not available
     * This really shouldn't be the case, but apparently some devices
       are misconfigured and are overriding the GPU-based CMHW
       implementation which would ensure the basic capabilities are
       available.
    
    Change-Id: I9f72cb58c9bbb63e0fdec4085b17a089f9d50174

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -85,6 +85,8 @@ import java.util.List;
 import com.android.settings.Utils;
 import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.dashboard.DashboardContainerView;
+
+import cyanogenmod.hardware.LiveDisplayManager;
 import cyanogenmod.providers.CMSettings;
 
 public class DisplaySettings extends SettingsPreferenceFragment implements
@@ -112,6 +114,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
     private static final String KEY_NOTIFICATION_LIGHT = "notification_light";
     private static final String KEY_BATTERY_LIGHT = "battery_light";
+    private static final String KEY_LIVEDISPLAY = "live_display";
 
     private static final String DASHBOARD_COLUMNS = "dashboard_columns";
 
@@ -120,6 +123,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private ListPreference mLcdDensityPreference;
     private FontDialogPreference mFontSizePref;
     private PreferenceScreen mDisplayRotationPreference;
+    private PreferenceScreen mLiveDisplayPreference;
 
     private static final String KEY_DOZE_CATEGORY = "category_doze_options";
     private static final String KEY_DOZE = "doze";
@@ -181,6 +185,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         mAccelerometer = (SwitchPreference) findPreference(DisplayRotation.KEY_ACCELEROMETER);
         if (mAccelerometer != null) {
             mAccelerometer.setPersistent(false);
+        }
+
+        mLiveDisplayPreference = (PreferenceScreen) findPreference(KEY_LIVEDISPLAY);
+        if (!LiveDisplayManager.getInstance(getActivity()).getConfig().isAvailable()) {
+            displayPrefs.removePreference(mLiveDisplayPreference);
         }
 
         mScreenSaverPreference = findPreference(KEY_SCREEN_SAVER);
