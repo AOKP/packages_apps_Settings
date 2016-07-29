87da9cb51f537425352340c7602342e26820a7e4
LorDClockaN <lordclockan@gmail.com>
2016-07-29 13:28:21 +0200

    SeekBarPreferenceCham: Color ProgressThumb icon to red on default value
    PS3: make the color themable
    
    Change-Id: Ia733cc3e4a59df890f8bdf53a34efa966db470be

--- src/com/android/settings/widget/SeekBarPreferenceCham.java
+++ src/com/android/settings/widget/SeekBarPreferenceCham.java
@@ -2,6 +2,8 @@ package com.android.settings.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.preference.Preference;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -35,6 +37,7 @@ public class SeekBarPreferenceCham extends Preference implements SeekBar.OnSeekB
     private TextView mTitle;
     private ImageView mImagePlus;
     private ImageView mImageMinus;
+    private Drawable mProgressThumb;
 
     private TextView mStatusText;
 
@@ -130,6 +133,7 @@ public class SeekBarPreferenceCham extends Preference implements SeekBar.OnSeekB
                     return true;
                 }
             });
+            mProgressThumb = mSeekBar.getThumb();
         }
         catch(Exception e)
         {
@@ -204,10 +208,14 @@ public class SeekBarPreferenceCham extends Preference implements SeekBar.OnSeekB
         }
         // change accepted, store it
         mCurrentValue = newValue;
-        if (mCurrentValue == mDefaultValue && mDefaultValue != -1)
+        if (mCurrentValue == mDefaultValue && mDefaultValue != -1) {
             mStatusText.setText(R.string.default_string);
-        else
+            int redColor = getContext().getResources().getColor(R.color.seekbar_dot_color);
+            mProgressThumb.setColorFilter(redColor, PorterDuff.Mode.SRC_IN);
+        } else {
             mStatusText.setText(String.valueOf(newValue));
+            mProgressThumb.clearColorFilter();
+        }
         persistInt(newValue);
     }
 
