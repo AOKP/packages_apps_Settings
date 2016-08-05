01f2d8cfb4ea0c76bac14ebd7857b53bec026f90
LorDClockaN <lordclockan@gmail.com>
2016-08-05 17:30:19 +0200

    [2/2]Settings: halo
    AICPfy:
    Brought to AICP by @LorDClockaN
    Settings part moved to AicpExtras
    
    Change-Id: I31d8b3aa26ae80fccae5abfe6752297ed373223e
    Signed-off-by: thecrazyskull <anaskarbila@aospa.co>

--- src/com/android/settings/notification/NotificationBackend.java
+++ src/com/android/settings/notification/NotificationBackend.java
@@ -46,6 +46,7 @@ public class NotificationBackend {
         row.banned = getNotificationsBanned(row.pkg, row.uid);
         row.priority = getHighPriority(row.pkg, row.uid);
         row.peekable = getPeekable(row.pkg, row.uid);
+        row.halo = getHalo(row.pkg, row.uid);
         row.sensitive = getSensitive(row.pkg, row.uid);
         return row;
     }
@@ -118,6 +119,25 @@ public class NotificationBackend {
         }
     }
 
+    public boolean getHalo(String pkg, int uid) {
+        try {
+            return sINM.isPackageAllowedForHalo(pkg, uid);
+        } catch (Exception e) {
+            Log.w(TAG, "Error calling NoMan", e);
+            return false;
+        }
+    }
+
+    public boolean setHalo(String pkg, int uid, boolean halo) {
+        try {
+            sINM.setHaloPolicyBlack(pkg, uid, halo);
+            return true;
+        } catch (Exception e) {
+           Log.w(TAG, "Error calling NoMan", e);
+           return false;
+        }
+    }
+
     public boolean setSensitive(String pkg, int uid, boolean sensitive) {
         try {
             sINM.setPackageVisibilityOverride(pkg, uid,
@@ -162,6 +182,7 @@ public class NotificationBackend {
         public boolean banned;
         public boolean priority;
         public boolean peekable;
+        public boolean halo;
         public boolean sensitive;
         public boolean first;  // first app in section
     }
