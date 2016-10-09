f5d4ec9e2e57c0a08a6c6db5a905d528eaaa4202
LorDClockaN <lordclockan@gmail.com>
2016-10-09 20:02:51 +0200

    Settings - Make CMSettingsSwitch classes API24 compatible
    Change-Id: I9607f69a4e50d20992c6df9616b47d64f8bc1c86

--- src/com/android/settings/cyanogenmod/SystemSettingSwitchPreference.java
+++ src/com/android/settings/cyanogenmod/SystemSettingSwitchPreference.java
@@ -17,7 +17,7 @@
 package com.android.settings.cyanogenmod;
 
 import android.content.Context;
-import android.preference.SwitchPreference;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import android.util.AttributeSet;
 
