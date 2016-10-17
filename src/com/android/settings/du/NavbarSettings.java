a3b8d1e6ee09ab20254a37adee5bdbcf57a4293d
LorDClockaN <lordclockan@gmail.com>
2016-10-17 15:37:52 +0200

    Settings - Add summary to Navbar dashboard item
    Change-Id: Ibdac0a6647e626af4b333d591c36c9675af4cc50

--- src/com/android/settings/du/NavbarSettings.java
+++ src/com/android/settings/du/NavbarSettings.java
@@ -18,7 +18,9 @@ package com.android.settings.du;
 
 import java.util.ArrayList;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
@@ -38,9 +40,10 @@ import com.android.internal.utils.du.ActionConstants;
 import com.android.internal.utils.du.Config;
 import com.android.internal.utils.du.DUActionUtils;
 import com.android.internal.utils.du.Config.ButtonConfig;
-import com.android.settings.R;
 
 import com.android.settings.du.preference.CustomSeekBarPreference;
+import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.R;
 
 public class NavbarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
@@ -164,4 +167,31 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
     public int getMetricsCategory() {
         return MetricsEvent.APPLICATION;
     }
+
+    private static class SummaryProvider implements SummaryLoader.SummaryProvider {
+
+        private final Context mContext;
+        private final SummaryLoader mSummaryLoader;
+
+        public SummaryProvider(Context context, SummaryLoader summaryLoader) {
+            mContext = context;
+            mSummaryLoader = summaryLoader;
+        }
+
+        @Override
+        public void setListening(boolean listening) {
+            if (listening) {
+                mSummaryLoader.setSummary(this, mContext.getString(R.string.summary_navbar));
+            }
+        }
+    }
+
+    public static final SummaryLoader.SummaryProviderFactory SUMMARY_PROVIDER_FACTORY
+            = new SummaryLoader.SummaryProviderFactory() {
+        @Override
+        public SummaryLoader.SummaryProvider createSummaryProvider(Activity activity,
+                                                                   SummaryLoader summaryLoader) {
+            return new SummaryProvider(activity, summaryLoader);
+        }
+    };
 }
