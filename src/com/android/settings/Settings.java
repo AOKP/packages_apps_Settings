6f0ef125bd8cfacc38e2a4f2661ff818202868a7
tsubus <tsubus@ilwt.org>
2017-11-19 09:18:40 +0000

    Settings: Add themes to dashboard
    - drawable ported from m AIM
    * forward port it to Oreo
    
    Change-Id: If8ecaa9599f2503bdf114b6dd59c45ee8ac4b05d
    Signed-off-by: Subhaam <derksoul98@gmail.com>
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -199,4 +199,7 @@ public class Settings extends SettingsActivity {
     public static class FlingSettingsActivity extends SettingsActivity { /* empty */ }
     public static class SmartbarSettingsActivity extends SettingsActivity { /* empty */ }
     public static class PulseSettingsActivity extends SettingsActivity { /* empty */ }
+
+    //Substratum
+    public static class ThemesActivity extends SettingsActivity {}
 }
