3e2346c4ae1c19a7f6e2f827602d314425b89104
Tom Marshall <tdm.code@gmail.com>
2018-03-24 02:54:09 +0100

    Settings: Anonymous stats
     * Backport anonymous user-counting from nougat.
    
     * Original work by:
       - Chris Soyars
       - Danesh Mondegarian
       - Roman Birg
       - Steve Kondik
       - Ricardo Cerqueira
       - Adnan Begovic
    
    Change-Id: I172c2ecd6a72fa2d0d9b1075525a19a7d0dd1f32

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -130,4 +130,5 @@ public class Settings extends SettingsActivity {
     public static class NotificationManagerActivity extends SettingsActivity { /* empty */ }
     public static class WeatherProviderServicesActivity extends SettingsActivity { /* empty */ }
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class AnonymousStatsActivity extends Settings { /* empty */ }
 }
